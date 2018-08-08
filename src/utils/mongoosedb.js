82278e3c20d8efb0f9d90c2ade18796eb5c21cdc
unknown <leo.shishir105@gmail.com>
2018-08-08 15:52:56 +0530

    mongodb mLab connection

--- src/utils/mongoosedb.js
+++ src/utils/mongoosedb.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose')
 
 module.exports = (app) => {
-    mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/mevn');
+    //mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://mevn_user:mevn123<dbpassword>@ds115442.mlab.com:15442/mevn_db' || 'mongodb://localhost:27017/mevn');
+    mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://mevn_user:mevn123@ds115442.mlab.com:15442/mevn_db');
     var db = mongoose.connection;
     db.on("error", console.error.bind(console, "connection error"));
     db.once("open", function(callback){
